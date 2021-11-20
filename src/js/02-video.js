bced6f7b305063a1bbd36156c3a86b6d7f886720
Yulia-kom <85898864+Yulia-kom@users.noreply.github.com>
2021-11-20 22:27:48 +0200

    hv8-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,17 @@
 
 import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
-const iframe = document.querySelector('iframe');
+const iframe = document.getElementById("vimeo-player-x");
+
 const player = new Player(iframe);
 
-player.on('play', function () {
-    console.log('played the video!');
-});
+const onPlay = (time) => localStorage.setItem("videoplayer-current-time", time.seconds);
 
-player.getVideoTitle().then(function (title) {
-    console.log('title:', title);
-});
+const currentTimeOfVideo = localStorage.getItem("videoplayer-current-time");
 
+(currentTimeOfVideo) ? player.setCurrentTime(currentTimeOfVideo) : null;
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+localStorage.clear();
